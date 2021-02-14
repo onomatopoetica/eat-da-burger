e95cc0940e2381708a1aca9c755970bafd61323a
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-14 15:09:32 -0700

    Adding HTML jumbotron + img to index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    cat.create(["name", "devoured"], [req.body.name, req.body.devoured], function (result) {
+    burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function (result) {
         // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    cat.update(
+    burger.update(
         {
             devoured: req.body.devoured
         },
