de02d360c9c5150539178907c97d3f399e91e70f
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-14 15:00:44 -0700

    Created all files + test HTML layout

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+### Schema
+
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(50) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guac Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheesy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
