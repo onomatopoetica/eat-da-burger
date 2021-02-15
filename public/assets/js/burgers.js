99815058bb3796273d01fc7c90abced6ca9a602f
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-15 13:58:48 -0700

    Updated hbs button + README

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,19 +2,19 @@
 $(function () {
     $(".change-sleep").on("click", function (event) {
         var id = $(this).data("id");
-        var newSleep = $(this).data("newsleep");
+        var newDevoured = $(this).data("newdevoured");
 
-        var newSleepState = {
-            sleepy: newSleep
+        var newDevouredState = {
+            devoured: newDevoured
         };
 
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
-            data: newSleepState
+            data: newDevouredState
         }).then(
             function () {
-                console.log("changed sleep to", newSleep);
+                console.log("changed devoured to", newDevoured);
                 // Reload the page to get the updated list
                 location.reload();
             }
