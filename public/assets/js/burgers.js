c58dfa70eaf6c13ed6c5ef0643c4b0dbbdbb9f86
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-16 14:48:14 -0700

    Fxd id/class field + added console.log

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,14 +1,14 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+// Make sure we wait to attach our handlers until the DOM is fully loaded
 $(function () {
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
 
         var newBurger = {
-            burger_name: $("#burger").val().trim(),
+            burger_name: $("#newburger").val().trim(),
             devoured: 0
         };
 
-        // Send the POST request
+        // Send the POST request to add a new burger
         $.ajax("/api/burgers/", {
             type: "POST",
             data: newBurger
@@ -41,12 +41,16 @@ $(function () {
     });
 
     // Deleting burger
-    $(".deleteBurger").on("click", function (event) {
+    $(".deleteburger").on("click", function (event) {
         event.preventDefault();
         var id = $(this).data("id");
         $.ajax({
             type: "DELETE",
             url: "/api/burgers/" + id
-        }).then(location.reload());
+        }).then(function () {
+            console.log("Burger deleted!");
+            // Reload the page to get the updated list
+            location.reload();
+        });
     })
 });
