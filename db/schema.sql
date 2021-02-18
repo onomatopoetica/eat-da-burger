82affd25f22d588050baa9238dbb524964699fd0
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-02-18 10:17:51 -0700

    Rmvd favicon

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(50) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
